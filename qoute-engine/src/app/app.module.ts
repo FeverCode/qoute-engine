cd7c1124af81ab74f745a1e8ba009faa968532fc
FeverCode <giddylancs.gl@gmail.com>
2022-04-10 19:07:11 +0300

    added directive

--- qoute-engine/src/app/app.module.ts
+++ qoute-engine/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { QuoteComponent } from './quote/quote.component';
 import { DetailsComponent } from './details/details.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     QuoteComponent,
-    DetailsComponent
+    DetailsComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,FormsModule,
